2e652305a98e926d5d219191f86ebb88a46158c1
Kamil Drzazga <kdrzazga@akamai.com>
2017-07-16 00:37:05 +0200

    C'mon lady

--- src/controller/AudioController.java
+++ src/controller/AudioController.java
@@ -1,21 +1,40 @@
 package controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javax.activation.DataHandler;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import com.ibm.websphere.jaxrs20.multipart.IAttachment;
+import com.ibm.websphere.jaxrs20.multipart.IMultipartBody;
+import com.ibm.wsdl.util.IOUtils;
+
 import javax.ws.rs.core.Response.Status;
 
 import bean.Audio;
 import bean.PostMessage;
 import bean.User;
+import filter.BasicSecurityContext;
 
 @Path("audios")
 @Stateless
@@ -27,8 +46,8 @@ public class AudioController {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response createAudioResource(Audio audio, @Context HttpServletRequest request) {
-		User user = (User) request.getAttribute("user");
+	public Response createAudioResource(Audio audio, @Context SecurityContext securityContext) {
+		User user = (User) ((BasicSecurityContext) securityContext).getUser();
 		audio.sanitize();
 		audio.setUser(user);
 		if (!em.contains(audio)) {
@@ -37,6 +56,27 @@ public class AudioController {
 		em.persist(audio);
 		em.flush();
 		return Response.status(Status.CREATED).entity(new PostMessage(audio.getId())).build();
+	}
+	
+	@Path("{id}")
+	@PUT
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String replaceAudio(
+			IMultipartBody multipartBody,
+			@Context SecurityContext securityContext,
+			@PathParam("id") String id
+	) {
+		try {
+			IAttachment attachment = multipartBody.getRootAttachment();
+			DataHandler dataHandler = attachment.getDataHandler();
+			InputStream audioStream = dataHandler.getInputStream();
+			Files.copy(audioStream, Paths.get("./test.wav"), StandardCopyOption.REPLACE_EXISTING);
+			return attachment.getContentType().toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "bad, bad error!";
+		}
 	}
 
 }
