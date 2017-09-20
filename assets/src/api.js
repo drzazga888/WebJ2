5f86c3cde9a6661615df490ed4b43179a5db68bc
Kamil Drzazga <kdrzazga@akamai.com>
2017-09-20 10:24:55 +0200

    base path to relative (for webpack)

--- assets/src/api.js
+++ assets/src/api.js
@@ -1,7 +1,7 @@
 import { jsonOrThrow, blobOrThrow, mergeArrayBufferWithForm, getBasicAuthorization } from './converters'
 
-//const basePath = '/webj2/api/'
-const basePath = 'https://localhost:9443/webj2/api/'
+const basePath = '/webj2/api/'
+//const basePath = 'https://localhost:9443/webj2/api/'
 
 // user
 
